ee28c75f28ba63917a0bdb3988bf52827ae3b362
Hassan Azzam <hassanazzam95@gmail.com>
2015-04-26 00:19:14 +0200

    Player Stick Movement UPD#1

--- AirHockey/AirHockey/Player_Stick.cs
+++ AirHockey/AirHockey/Player_Stick.cs
@@ -11,28 +11,36 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
 
 
 namespace AirHockey
 {
     public class Player_Stick : Stick
     {
-        public Player_Stick(NewGame game) : base(game)
+        Vector2 PreviousMousePostion;
+        public Player_Stick(NewGame game)
+            : base(game)
         {
             Velocity = new Vector2(0, 0);
+            PreviousMousePostion = new Vector2(0, 0);
         }
 
         public void LoadContent()
         {
-            Position = new Vector2(70, Table.HEIGHT / 2);
+            Position = new Vector2(game.GameTable.TableTopLeft.X + Table.WIDTH - 70 - RADIUS, game.GameTable.TableTopLeft.Y + Table.HEIGHT / 2);
             base.LoadContent();
         }
 
         override public void Movement()
         {
-            Position.X = Math.Min(Position.X, (Table.WIDTH / 2) - RADIUS);
+            MouseState MOUSE = Mouse.GetState();
+            Vector2 CurrentMousePosition = new Vector2(MOUSE.X, MOUSE.Y);
+            Velocity = (CurrentMousePosition - PreviousMousePostion);
             base.Movement();
+            PreviousMousePostion = CurrentMousePosition;
+            Position.X = Math.Min(Position.X, (Table.WIDTH / 2) - RADIUS);
+            base.Hit();
         }
-
     }
 }
