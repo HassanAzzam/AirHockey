b28c6b85fe57987668f73a368800e20d690ad641
Ayman Zaki <ayman.zaki.az@gmail.com>
2015-05-05 00:13:07 +0200

    Menu & Goal

--- AirHockey/AirHockey/Table.cs
+++ AirHockey/AirHockey/Table.cs
@@ -19,12 +19,14 @@ namespace AirHockey
         public Vector2 TableTopLeft;
         public const float FRICTION=0.1F;
         NewGame game;
+
         public Table(NewGame game) : base(game)
         {
             this.game = game;
             game.Content.RootDirectory = "Content";
             TableTopLeft = new Vector2(0, 0);
         }
+
         protected override void LoadContent()
         {
             TEXTURE = game.Content.Load<Texture2D>("Table");
@@ -32,6 +34,7 @@ namespace AirHockey
             WIDTH=TEXTURE.Width;
             base.LoadContent();
         }
+
         public void Draw(){
             game.spriteBatch.Draw(this.TEXTURE, TableTopLeft, Color.White);
         }
